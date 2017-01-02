19f00637aa0d8fb43e3a0fd8e610e1bf4e95c850
unknown <medcomcr@gmail.com>
2017-01-02 21:34:29 +0200

    'init'

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = function(config) {
+
+	config = config || {};
+
+	config.queryFormat = function (query, values) {
+	  	if (!values) return query;
+	  	return query.replace(/(\?)?\?(\w+)/g, function (txt, mark, key) {
+	    		if (values.hasOwnProperty(key)) {
+	    		        var value = values[key];
+	      			return mark ? this.escapeId(value) : this.escape(value);
+	    		}
+	    		return txt;
+	  	}.bind(this));
+	};
+
+//module.exports.get = function(){
+    //console.log(config);
+//}
+
+	return {
+		get : get,
+		config : config
+	};
+
+	function get(){
+}
+
+}
+
+
