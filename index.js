877b9f84f91edf6192c8599fe8df70c87697a806
medcomya <medcomcr@yandex.ru>
2017-01-04 23:32:18 +0200

    added getlist

--- index.js
+++ index.js
@@ -1,102 +1,177 @@
+var mysql = require('mysql');
+var queries = require('./queries');
 
-var mysql      = require('mysql');
-var queries      = require('queries');
+module.exports = function (config) {
 
-module.exports = function(config) {
-
-	config = config || {};
-	config.connectionLimit = config.connectionLimit || 10;
+    config = config || {};
+    config.connectionLimit = config.connectionLimit || 10;
     //
-	//config.queryFormat = function (query, values) {
-	//  	if (!values) return query;
-	//  	return query.replace(/(\?)?\?(\w+)/g, function (match, mark, key) {
-	//    		if (values.hasOwnProperty(key)) {
-	//    		        var value = values[key];
-	//      			return mark ? this.escapeId(value) : this.escape(value);
-	//    		}
-	//    		return match;
-	//  	}.bind(this));
-	//};
+    //config.queryFormat = function (query, values) {
+    //  	if (!values) return query;
+    //  	return query.replace(/(\?)?\?(\w+)/g, function (match, mark, key) {
+    //    		if (values.hasOwnProperty(key)) {
+    //    		        var value = values[key];
+    //      			return mark ? this.escapeId(value) : this.escape(value);
+    //    		}
+    //    		return match;
+    //  	}.bind(this));
+    //};
 
-	var pool  = mysql.createPool(config);
+    var pool = mysql.createPool(config);
+
+    function close() {
+        pool.end(function (err) {
+        });
+    }
+
+    function expressionArg(val, prefix, postfix){
+        var expression = '?? ' + val + '?';
+        return function(arg){
+            if(prefix){
+                arg = prefix + arg;
+            }
+            if(postfix){
+                arg = arg + postfix;
+            }
+
+            return function(key){
+                return mysql.format(expression, [key, arg]);
+            }
+        };
+    }
+
+    var expressions = {
+        equal: expressionArg('='),
+        notEqual: expressionArg('<>'),
+        above: expressionArg('>'),
+        aboveAndEqual: expressionArg('>='),
+        below: expressionArg('<'),
+        belowAndEqual: expressionArg('<='),
+
+        startWith: expressionArg('like', '', '%'),
+        endWith: expressionArg('like', '%'),
+        contains: expressionArg('like', '%', '%'),
+
+        and: 0,
+        or: 1
+    };
+
+    return {
+        get: get,
+        getList: getList,
+        close: close,
+        config: config,
+        expressions: expressions
+    };
 
 
 
-	return {
-		get : get,
-		config : config
-	};
+    function init(options) {
 
-	function get(options, id){
-		return processQuery(function () {
-			var errors;
-			if(!options) {
-				errors = 'Invalid args';
-			} else {
-				if(typeof options === 'function'){
-					options = options();
-				}
-			}
+        if (!options) {
+            return {
+                error : 'Invalid args'
+            };
+        }
 
-			return {
-				init:init,
-				query: queries.get,
-				params: [options.objectFields, options.table, options.keyFiled, id]
-			};
+        if (typeof options === 'function') {
+            return new options();
+        }
 
-			function init(){
-				return error;
-			}
-			
-		});
-	}
+        return options;
+    }
 
-	function processQuery(queryOptions){
-                   return new Promise(function(resolve, reject){
+    function processOptions(options){
+        options = options || {
+            limit: 10
+        };
 
-			if(typeof queryOptions === 'function'){
-				queryOptions = queryOptions();
-			}
+        var operation = 0;
+        var where;
 
-			var error = queryOptions.init && queryOptions.init();
-			if(error){
-				reject(err);
-				return;	
-			}
-			pool.getConnection(function(err, connection) {
-				if (err) {
-					reject(err);
-					connection.release();
-					return;
-				}
-                                var queryFunction = function(err, result) {
-                                        if (err) {
-						reject(err);
-						connection.release();
-						return;
-					}
-					if(queryOptions.processResult) {
-	                                        result = queryOptions.processResult(result, reject);
-					}
-					connection.release();
-					resolve(rows);
-				}
-				if(queryOptions.params) {
-					connection.query(queryOptions.query, queryOptions.params, queryFunction);
-				} else {
-                                 	connection.query(queryOptions.query, queryFunction);
-				}
-			});
-		});
-	}
+        if(options.where){
+            var aWhere = [];
+            for(var key in options.where){
+                if(options.where.hasOwnProperty(key)) {
+                    if(key === 'operation') {
+                        operation = options.where[key];
+                    } else {
+                        aWhere.push(options.where[key](key));
+                    }
+                    //console.log("obj." + prop + " = " + obj[prop]);
+                }
+            }
 
+            where = aWhere.join(operation ? ' or ': ' and ');
+
+        }
+
+        if(!where){
+            where = '1=1';
+        }
+
+
+        return {
+            options : options,
+            where : where
+        };
+    }
+
+    function getList(type, options) {
+        return processQuery(function () {
+            type = init(type);
+            var ops = processOptions(options);
+            type.query = queries.getList + ops.where + ' limit 10';
+            type.params = [type.listFields, type.table];
+            return type;
+        });
+    }
+
+    function get(type, id) {
+        return processQuery(function () {
+            type = init(type);
+            type.query = queries.get;
+            type.params = [type.objectFields, type.table, type.keyFiled, id];
+            return type;
+        });
+    }
+
+    function processQuery(queryOptions) {
+        return new Promise(function (resolve, reject) {
+
+            if (typeof queryOptions === 'function') {
+                queryOptions = queryOptions();
+            }
+
+            var error = queryOptions.error;
+            if (error) {
+                reject(err);
+                return;
+            }
+            pool.getConnection(function (err, connection) {
+                if (err) {
+                    reject(err);
+                    connection.release();
+                    return;
+                }
+                var queryFunction = function (err, result) {
+                    if (err) {
+                        reject(err);
+                        connection.release();
+                        return;
+                    }
+                    if (queryOptions.processResult) {
+                        result = queryOptions.processResult(result, reject);
+                    }
+                    connection.release();
+                    resolve(result);
+                };
+                if (queryOptions.params) {
+                    connection.query(queryOptions.query, queryOptions.params, queryFunction);
+                } else {
+                    connection.query(queryOptions.query, queryFunction);
+                }
+            });
+        });
+    }
 };
-
-function User(){
-	return {
-		table: 'users',
-		keyFiled:'Id',
-		listFields: ['','','',''],
-		objectFields: ['','','','']
-	}
-}
